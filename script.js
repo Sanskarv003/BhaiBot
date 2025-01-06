6b9a1779937ba959e77bd91c802c2056aa914556
Sanskarv003 <167301979+Sanskarv003@users.noreply.github.com>
2025-01-06 13:49:49 +0530

    Update script.js

--- script.js
+++ script.js
@@ -8,7 +8,7 @@ const chatbotToggler = document.querySelector("#chatbot-toggler");
 const closeChatbot = document.querySelector("#close-chatbot");
 
 //API setup
-const API_KEY = "AIzaSyBW9t_GG_EwlfJZ8YJomm9cyihXPEQfg_c";
+const API_KEY = "YOUR_API_KEY";
 const API_URL = `https://generativelanguage.googleapis.com/v1beta/models/gemini-1.5-flash:generateContent?key=${API_KEY}`;
 
 const userData = {
